3931293a24458f42f2a91ce881201e398391d57d
Camilo Medina <medina1camilo@gmail.com>
2024-12-22 13:19:01 +0100

    master
    init PRICE table

--- src/test/java/co/medina/test/pricecatalogapi/PriceCatalogApiApplicationTests.java
+++ src/test/java/co/medina/test/pricecatalogapi/PriceCatalogApiApplicationTests.java
@@ -1,13 +1,23 @@
 package co.medina.test.pricecatalogapi;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PriceCatalogApiApplicationTests {
 
+    private PriceCatalogApiApplication application;
+
+    @BeforeEach
+    void setUp() {
+        application = new PriceCatalogApiApplication();
+    }
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(application);
     }
 
 }
