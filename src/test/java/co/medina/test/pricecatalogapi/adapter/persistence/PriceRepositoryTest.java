1f01f5955eae197d1e46eb7287fb4224494e2b0a
Camilo Medina <medina1camilo@gmail.com>
2024-12-23 11:58:57 +0100

    feature/filter
    unit tests

--- src/test/java/co/medina/test/pricecatalogapi/adapter/persistence/PriceRepositoryTest.java
+++ src/test/java/co/medina/test/pricecatalogapi/adapter/persistence/PriceRepositoryTest.java
@@ -21,11 +21,57 @@ class PriceRepositoryTest {
     }
 
     @Test
-    void shouldSaveAndRetrievePrice() {
-        LocalDateTime testDateTime = LocalDateTime.of(2020, 6, 14, 10, 0);
-        Price price = new Price(1, LocalDateTime.now(), LocalDateTime.now().plusDays(1), 1, 35455, 1, 40.00, "EUR");
-        priceRepository.save(price);
-        Optional<Price> priceResponse = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, testDateTime);
-        Assertions.assertNotNull(priceResponse);
+    void test1_findPrice_at_10am_on_14th() {
+        LocalDateTime requestDate = LocalDateTime.of(2020, 6, 14, 10, 0);
+        Optional<Price> price = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, requestDate);
+
+        Assertions.assertTrue(price.isPresent());
+        Assertions.assertEquals(1, price.get().getBrandId());
+        Assertions.assertEquals(35455, price.get().getProductId());
+        Assertions.assertEquals(35.50, price.get().getPrice().doubleValue());
+    }
+
+    @Test
+    void test2_findPrice_at_16pm_on_14th() {
+        LocalDateTime requestDate = LocalDateTime.of(2020, 6, 14, 16, 0);
+        Optional<Price> price = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, requestDate);
+
+        Assertions.assertTrue(price.isPresent());
+        Assertions.assertEquals(1, price.get().getBrandId());
+        Assertions.assertEquals(35455, price.get().getProductId());
+        Assertions.assertEquals(25.45, price.get().getPrice().doubleValue());
+    }
+
+    @Test
+    void test3_findPrice_at_21pm_on_14th() {
+        LocalDateTime requestDate = LocalDateTime.of(2020, 6, 14, 21, 0);
+        Optional<Price> price = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, requestDate);
+
+        Assertions.assertTrue(price.isPresent());
+        Assertions.assertEquals(1, price.get().getBrandId());
+        Assertions.assertEquals(35455, price.get().getProductId());
+        Assertions.assertEquals(35.50, price.get().getPrice().doubleValue());
+    }
+
+    @Test
+    void test4_findPrice_at_10am_on_15th() {
+        LocalDateTime requestDate = LocalDateTime.of(2020, 6, 15, 10, 0);
+        Optional<Price> price = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, requestDate);
+
+        Assertions.assertTrue(price.isPresent());
+        Assertions.assertEquals(1, price.get().getBrandId());
+        Assertions.assertEquals(35455, price.get().getProductId());
+        Assertions.assertEquals(30.50, price.get().getPrice().doubleValue());
+    }
+
+    @Test
+    void test5_findPrice_at_21pm_on_16th() {
+        LocalDateTime requestDate = LocalDateTime.of(2020, 6, 16, 21, 0);
+        Optional<Price> price = priceRepository.findFirstByBrandIdAndProductIdAndDate(1, 35455, requestDate);
+
+        Assertions.assertTrue(price.isPresent());
+        Assertions.assertEquals(1, price.get().getBrandId());
+        Assertions.assertEquals(35455, price.get().getProductId());
+        Assertions.assertEquals(38.95, price.get().getPrice().doubleValue());
     }
 }
